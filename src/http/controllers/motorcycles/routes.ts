1d18dac6664ab8158c968ae718e3c4ded4b7d325
Adones Balica <adonesbalica@gmail.com>
2026-09-17 10:27:10 -0300

    feat: add fetch motorcycles route and controller

--- src/http/controllers/motorcycles/routes.ts
+++ src/http/controllers/motorcycles/routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { create } from './create-motorcycle'
 import { fetchMotorcycleById } from './fetch-motorcycle-by-id'
+import { fetchMotorcycles } from './fetch-motorcyles'
 
 export async function motorcyclesRoutes(app: FastifyInstance) {
   app.post('/motorcycles', create)
+  app.get('/motorcycles', fetchMotorcycles)
   app.get('/motorcycles/:id', fetchMotorcycleById)
 }
