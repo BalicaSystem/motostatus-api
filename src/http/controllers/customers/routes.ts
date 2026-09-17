36276d842363d0ae1150c8e03a824559939f5022
Adones Balica <adonesbalica@gmail.com>
2026-09-17 18:28:25 -0300

    feat: implement fetch customers functionality with pagination and validation

--- src/http/controllers/customers/routes.ts
+++ src/http/controllers/customers/routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { createCustomer } from './create-customer'
+import { fetchCustomers } from './fetch-customers'
 
 export async function customersRoutes(app: FastifyInstance) {
   app.post('/customers', createCustomer)
+  app.get('/customers', fetchCustomers)
 }
