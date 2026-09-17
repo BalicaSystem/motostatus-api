79014d1af372e0c644a29a7514eac506a0b1d5d6
Adones Balica <adonesbalica@gmail.com>
2026-09-17 10:26:52 -0300

    feat: implement pagination, count and status update in drizzle repository

--- src/repositories/drizzle/drizzle-motorcycle-repository.ts
+++ src/repositories/drizzle/drizzle-motorcycle-repository.ts
@@ -1,13 +1,20 @@
-import { eq, desc } from 'drizzle-orm'
+import { eq, desc, count } from 'drizzle-orm'
 import { db } from '../../db'
 import {
   motorcycles,
   type Motorcycle,
   type NewMotorcycle,
 } from '../../db/schema'
-import type { MotorcyclesRepository } from '../motorcycles-repository'
+import type {
+  FindManyMotorcyclesParams,
+  MotorcyclesRepository,
+} from '../motorcycles-repository'
 
 export class DrizzleMotorcyclesRepository implements MotorcyclesRepository {
+  updateStatus(id: string, status: Motorcycle['status']): Promise<Motorcycle> {
+    throw new Error('Method not implemented.')
+  }
+
   async findById(id: string): Promise<Motorcycle | null> {
     const [motorcycle] = await db
       .select()
@@ -17,8 +24,19 @@ export class DrizzleMotorcyclesRepository implements MotorcyclesRepository {
     return motorcycle ?? null
   }
 
-  async findMany(): Promise<Motorcycle[]> {
-    return db.select().from(motorcycles).orderBy(desc(motorcycles.createdAt))
+  async findMany({ limit, offset }: FindManyMotorcyclesParams) {
+    return db
+      .select()
+      .from(motorcycles)
+      .orderBy(desc(motorcycles.createdAt))
+      .limit(limit)
+      .offset(offset)
+  }
+
+  async count() {
+    const [result] = await db.select({ count: count() }).from(motorcycles)
+
+    return result?.count ?? 0
   }
 
   async create(data: NewMotorcycle): Promise<Motorcycle | null> {
